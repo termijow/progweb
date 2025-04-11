ac67c847f11046a1430747fb4f96427e239a4afb
Juan <termijow@gmail.com>
2025-04-11 06:59:44 -0500

    aaaa

---
+++ javascriptprueba/script.js
@@ -0,0 +1,26 @@
+var name = "hola";
+let last_name = "que tal";
+const id = 1234;
+
+console.log(name);
+console.log(last_name);
+console.log(id);
+
+name = 12;
+last_name = true;
+
+function calcular(name, last_name) {
+    let texto = name;
+    return 12;
+}
+
+const my_funcion = () => {
+    console.log("arrow function");
+
+}
+
+let value = calcular();
+
+console.log(value);
+
+my_funcion();
